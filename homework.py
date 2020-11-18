2b1fc4c3d1cd0f79e49d52050cbbc2ad5730cdb9
Dudi001 <murad.manapov.96@gmail.com>
2020-11-18 19:08:38 +0300

    i home fix last misstakes

--- homework.py
+++ homework.py
@@ -5,12 +5,12 @@ class Record:
     DATE_FORMAT = '%d.%m.%Y'
     def __init__(self, amount, comment, date=None):
         self.amount = amount
-        self.comment = comment
-        
+        self.comment = comment 
         if date is None:
             self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, self.DATE_FORMAT).date()
+
 
 #Родительский класс.
 class Calculator: 
@@ -33,17 +33,20 @@ class Calculator:
         return sum(record.amount for record in self.records
                    if record.date == date_today)
     
-    #Расчет денег\калорий за последние 7 дней.
+    #Расчет денег и калорий за последние 7 дней.
     def get_week_stats(self):
         date_today = dt.date.today()
         week_delta = date_today - dt.timedelta(days=7)
         return sum(record.amount for record in self.records
-                   if date_today >= record.date >= week_delta)
+                   if  week_delta <= record.date <= date_today)
+
 
 #Калькулятор калорий.
 class CaloriesCalculator(Calculator):  
-    TEXT_CALORIES = ("Сегодня можно съесть что-нибудь ещё, "
-                 "но с общей калорийностью не более {value} кКал") 
+    TEXT_CALORIES = (
+        "Сегодня можно съесть что-нибудь ещё, "
+        "но с общей калорийностью не более {value} кКал"
+        ) 
     TEXT_2_CALORIES = "Хватит есть!"
 
     #Вывод количества калорий.
@@ -54,52 +57,56 @@ class CaloriesCalculator(Calculator):
         else:
             return self.TEXT_2_CALORIES
 
+
 #Калькулятор денег.
 class CashCalculator(Calculator):
-    RUB_RATE = float(1.0)
-    USD_RATE = float(60.0)
-    EURO_RATE = float(70.0)
+    RUB_RATE = 1.0
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
     
     CURRENCIES = {
         'rub': (RUB_RATE, 'руб'),
         'usd': (USD_RATE, 'USD'),
         'eur': (EURO_RATE, 'Euro')
     }
-    TEXT_CASH_REMAINS = ("На сегодня осталось {spent}"
-                "{currency_name}")
-    TEXT_DEBT_CASH = ("Денег нет, держись: твой долг - "
-                    "{spent} {currency_name}")
+    TEXT_CASH_REMAINS = (
+        "На сегодня осталось {spent} "
+        "{currency_name}"
+        )
+    TEXT_DEBT_CASH = (
+        "Денег нет, держись: твой долг - "
+        "{spent} {currency_name}"
+        )
     TEX_NO_CASH = ("Денег нет, держись")
-    TEXT_NO_CURRENCY = ("Данная валюта {no_currency} не поддерживается")
+    TEXT_NO_CURRENCY = (
+        "Данная валюта {no_currency} не поддерживается"
+        )
     
     #Конвертация валюты и условия вывода.
     def get_today_cash_remained(self, currency='rub'):
         #Обработка исключений.
         if currency not in self.CURRENCIES: 
             raise ValueError(self.TEXT_NO_CURRENCY.format(
-            	no_currency=currency
-            	))
+                no_currency=currency
+                ))
 
         remainder_day = self.day_remainder()
         
         if remainder_day == 0:
             return self.TEX_NO_CASH
-        
-        #Распаковываем словарь и округляем валюту до сотых.
+         
         rate, currency_name = self.CURRENCIES[currency]
-        spent_by_currency = round(remainder_day/ rate, 2)
-        
+        spent_by_currency = round(abs(remainder_day) / rate, 2)
         if remainder_day > 0:
             return self.TEXT_CASH_REMAINS.format(
-            	spent=spent_by_currency,
-            	currency_name=currency_name
-            	)
+                spent=spent_by_currency,
+                currency_name=currency_name
+                )
         else:
              return self.TEXT_DEBT_CASH.format(
-             	spent=spent_by_currency,
-            	currency_name=currency_name
-            	)
+                spent=spent_by_currency,
+                currency_name=currency_name
+                )
 
 if __name__ == '__main__':
     pass
-      
